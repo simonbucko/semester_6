eb843a690bf18802838949dabd06824ec784d34f
Šimon Bučko <sibu@queue-it.com>
2023-05-01 13:37:59 +0200

    add file python example

--- 23-multimedia-form/python/main.py
+++ 23-multimedia-form/python/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, File, UploadFile
 
 app = FastAPI()
 
 @app.post("/form")
 def form_handler(username:str = Form(...), password:str = Form(default=...,min_length=8)):
         return {"username": username}
+
+# parameter "file" must be the same as name in frontend
+# @app.post("fileform")
+# def fiel_form_handler(file:bytes = File(...)):
+#     with open("file.txt", "wb") as f:
+#         f.write(file)
+#         f.close()
+
+#     return {"message": "file uploaded"}
+
+@app.post("fileform")
+async def fiel_form_handler(file:UploadFile = File(...)):
+    contents = await file.read()
+    
+    return {"filename": file.filename}
