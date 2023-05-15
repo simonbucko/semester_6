c6a58c84f58c194f3e6d90d1499cc90b03fa657d
Šimon Bučko <sibu@queue-it.com>
2023-05-15 11:22:59 +0200

    crawling

--- 24-webscraping/python/data-scraper/data_scraper.py
+++ 24-webscraping/python/data-scraper/data_scraper.py
@@ -2,16 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 html = requests.get("https://en.wikipedia.org/wiki/List_of_Monty_Python_projects").text
-parsed_html = BeautifulSoup(html, "lxml") #lxml is a perser type which has the most features
+parsed_html = BeautifulSoup(html, "lxml")
 
-tags = parsed_html.find("div", {"class":"mw-parser-output"}) # get all div by class
+tags = parsed_html.find("div", { "class": "mw-parser-output" })
 
 projects = {}
 
+current_category = None
+
 for tag in tags:
     if tag.name == "h2":
-        current_category = tag.text.replace("[edit]","")
-        print("="*40)
-        print(current_category)
-        print("="*40)
-    else:
+        current_category = tag.text.replace("[edit]", "")
+        projects[current_category] = []
+    elif tag.name == "ul":
+        for li in tag.find_all("li"):
+            projects[current_category].append(li.text)
+
+del projects["Notes"]
+del projects["References"]
+del projects["Further reading"]
+
+from pprint import pprint
+pprint(projects)
